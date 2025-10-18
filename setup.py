d1cae4ed8fd1d603161e4ad269049f6c78b9e924
Arkan Ataya <snurlaelah163@gmail.com>
2025-10-18 13:08:16 +0700

    1.0.1 - Just Small Changes

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="bitmicro",  # your PyPI package name
-    version="1.0.0",  # increment this for new releases
+    version="1.0.1",  # increment this for new releases
     author="Neoncorp",
     author_email="snurlaelah163@gmail.com",
     description="MicroBitcoin Python library (Bitcash-style)",
